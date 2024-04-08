0e4bc8610a56ecd677bc450ed629fbf912319628
sajan moon <sajanmoon5@gmail.com>
2024-04-08 20:52:57 +0530

    all setup configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,11 @@
 const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  return (req, res, next) => {
+    try {
+      Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+    } catch (err) {
+      next(err); // Pass any synchronous errors to the error handling middleware
+    }
   };
 };
+
 export { asyncHandler };
